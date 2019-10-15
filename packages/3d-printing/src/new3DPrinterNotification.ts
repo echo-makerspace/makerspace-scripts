043ee4c7707bd0e9f4d1371a790c028a0161188d
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-10-15 17:15:10 +0200

    Create scripts for 3d-printing course requests

---
+++ packages/3d-printing/src/new3DPrinterNotification.ts
@@ -0,0 +1,141 @@
+const slackWebhook = PropertiesService.getScriptProperties().getProperty("PROD_WEBHOOK");
+const channel = PropertiesService.getScriptProperties().getProperty("PROD_CHANNEL");
+const url = PropertiesService.getScriptProperties().getProperty("MAIN_SHEET_URL");
+
+interface Answers {
+  timestamp: Date;
+  name: String;
+  email: String;
+}
+
+interface Response {
+  authMode: Object;
+  values: String[];
+  namedValues: [Object];
+  range: Object;
+  source: Object;
+  triggerUid: number;
+}
+
+// To activate the script, run `initialize` once in the Script Editor.
+function initialize() {
+  const triggers = ScriptApp.getProjectTriggers();
+  triggers.forEach(trigger => {
+    ScriptApp.deleteTrigger(trigger);
+  });
+
+  ScriptApp.newTrigger("sendToSlack")
+    .forSpreadsheet(SpreadsheetApp.getActiveSpreadsheet())
+    .onFormSubmit()
+    .create();
+}
+
+// Get headers that applicant fills in using Google Forms.
+function getHeaders() {
+  const sheet = SpreadsheetApp.getActiveSheet();
+  const headers = sheet.getRange(1, 1, 1, 3);
+  const names = headers.getValues()[0];
+
+  return names;
+}
+
+// Create the blocks used in the message sent to Slack, see their API for more documentation.
+function constructMessage(answers: Answers) {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet();
+  const secondaryURL = sheet.getUrl();
+
+  const blocks = [
+    {
+      type: "section",
+      block_id: "newMember",
+      text: {
+        type: "mrkdwn",
+        text: `<!channel> Noen vil ha 3D-printerkurs! :partyparrot:\n*${answers.name}*`
+      }
+    },
+    {
+      type: "section",
+      block_id: "about",
+      fields: [
+        {
+          type: "mrkdwn",
+          text: `*Dato*\n<!date^${Math.floor(
+            answers.timestamp.valueOf() / 1000
+          )}^{time}, {date}|${answers.timestamp.toISOString()}>`
+        },
+        {
+          type: "mrkdwn",
+          text: "*E-post*\n" + answers.email
+        }
+      ]
+    },
+    {
+      type: "actions",
+      block_id: "actions",
+      elements: [
+        {
+          type: "button",
+          style: "primary",
+          text: {
+            type: "plain_text",
+            text: "Gå til 3D-skjema"
+          },
+          url: secondaryURL
+        },
+        {
+          type: "button",
+          style: "primary",
+          text: {
+            type: "plain_text",
+            text: "Gå til hovedskjema"
+          },
+          url: url
+        }
+      ]
+    }
+  ];
+
+  return blocks;
+}
+
+// Send message to our Slack instance.
+function sendToSlack(response: Response) {
+  if (slackWebhook === null) {
+    return;
+  }
+
+  const answers = convertToAnswers(response);
+  const message = constructMessage(answers);
+  const payload = { text: "Vi har fått et nytt medlem!", blocks: message };
+  const options = {
+    method: "post",
+    payload: JSON.stringify(payload)
+  };
+
+  let resp = UrlFetchApp.fetch(slackWebhook, options);
+  Logger.log(resp);
+}
+
+// Get the answers from the applicant to the form.
+function convertToAnswers(response: Response): Answers {
+  const answers: Answers = {
+    timestamp: convertToDate(response.values[0]),
+    email: response.values[1],
+    name: response.values[2]
+  };
+
+  return answers;
+}
+
+// Because fuck date parsing.
+function convertToDate(input: String): Date {
+  const [d1, d2] = input.split(" ");
+  const [day, month, year] = d1.split("/");
+
+  if (!d2) {
+    return new Date(parseInt(year), parseInt(month), parseInt(day));
+  }
+
+  const [hour, minute, second] = d2.split(":");
+  return new Date(parseInt(year), parseInt(month), parseInt(day), parseInt(hour), parseInt(minute), parseInt(second));
+}
